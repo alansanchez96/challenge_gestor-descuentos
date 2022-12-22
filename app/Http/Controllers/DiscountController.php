715cf06721f4576702ff38cd5e1b26633bfbbf65
Alan Sanchez <alaansannchezz@gmail.com>
2022-12-22 19:48:29 -0300

    feat: Se finalizo el formulario

--- app/Http/Controllers/DiscountController.php
+++ app/Http/Controllers/DiscountController.php
@@ -31,30 +31,53 @@ class DiscountController extends Controller
         return view('discount.create', compact('brands', 'accessTypes', 'regions'));
     }
 
-    public function store(DiscountRequest $request)
+    public function store(Request $request)
     {
         $discount = new Discount();
-        $discountRanges = new DiscountRange();
-
         $discount->name = $request->discounts_name;
         $discount->priority = $request->discounts_priority;
-        $discount->start_date = /* $request->start_date; */ '2022-08-02';
-        $discount->end_date = /* $request->end_date; */     '2022-08-02';
+        $discount->start_date = $request->start_date;
+        $discount->end_date = $request->end_date;
         $discount->active = $request->discounts_active;
         $discount->region_id = $request->discounts_region_id;
         $discount->brand_id = $request->discount_brand_id;
         $discount->access_type_code = $request->discounts_access_type_code;
         $discount->save();
 
+        $discountModel = Discount::where('name', $request->discounts_name)->get();
+
+        $discountRanges = new DiscountRange();
         $discountRanges->from_days = $request->from_days_1;
         $discountRanges->to_days = $request->to_days_1;
         $discountRanges->code = $request->discount_code_1;
         $discountRanges->discount = $request->discount_percent_1;
-
-        $discountModel = Discount::where('name', $request->discounts_name)->get();
-        
         $discountRanges->discount_id = $discountModel[0]->id;
+
         $discountRanges->save();
+
+        if ($request->from_days_2 && $request->to_days_2 !== null) {
+            $discountRanges_2 = new DiscountRange();
+            $discountRanges_2->from_days = $request->from_days_2;
+            $discountRanges_2->to_days = $request->to_days_2;
+            $discountRanges_2->code = $request->discount_code_2;
+            $discountRanges_2->discount = $request->discount_percent_2;
+            $discountRanges_2->discount_id = $discountModel[0]->id;
+
+            $discountRanges_2->save();
+        }
+
+        if ($request->from_days_3 && $request->to_days_3 !== null) {
+            $discountRanges_3 = new DiscountRange();
+            $discountRanges_3->from_days = $request->from_days_3;
+            $discountRanges_3->to_days = $request->to_days_3;
+            $discountRanges_3->code = $request->discount_code_3;
+            $discountRanges_3->discount = $request->discount_percent_3;
+            $discountRanges_3->discount_id = $discountModel[0]->id;
+
+            $discountRanges_3->save();
+        }
+
+
 
         return $discount;
     }
